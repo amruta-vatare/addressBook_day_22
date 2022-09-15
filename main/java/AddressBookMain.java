ffc296d9a638d741b82f992f43c9fc870c985301
amruta.vatare <vatareamruta615@gmail.com>
2022-09-15 19:54:31 +0530

    Edit function is implemented

--- main/java/AddressBookMain.java
+++ main/java/AddressBookMain.java
@@ -7,7 +7,7 @@ public class AddressBookMain {
     private List<Person> contact = new ArrayList<>();
     private AddressBook book = new AddressBook();
 
-    public List<Person> createContact(){
+    public Person createContact(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first name");
         String fName = sc.next();
@@ -26,15 +26,78 @@ public class AddressBookMain {
         System.out.println("Enter email name");
         String email = sc.next();
         Person p = new Person(fName,lName,address,city,state,zipCode,phoneNo,email);
-        contact.add(p);
-        return contact;
+        return p;
     }
     public static void main(String[] args) {
-        AddressBookMain main = new AddressBookMain();
         System.out.println("Welcome to address book program");
-        List<Person> personList = main.createContact();
-        main.addToAddressBook(personList);
-        main.display();
+        Scanner sc = new Scanner(System.in);
+        AddressBookMain main = new AddressBookMain();
+        int choice = 1;
+        do {
+            System.out.println("Enter the your choice");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Edit Contact");
+            System.out.println("3. Delete Contact");
+            System.out.println("4. Display Contact");
+            choice = sc.nextInt();
+            switch (choice){
+                case 1:
+                    Person p = main.createContact();
+                    main.addToContactList(p);
+                    main.addToAddressBook(main.contact);
+                    break;
+                case 2:
+                    main.editContact();
+                    break;
+                case 3:
+                    main.deleteContact();
+                    break;
+                case 4:
+                    main.display();
+                    break;
+                default:
+                    break;
+            }
+        }while (choice!=0);
+
+    }
+
+    private void deleteContact() {
+    }
+
+    private void editContact() {
+        Scanner sc = new Scanner(System.in);
+        display();
+        System.out.println("Enter the name of which you want to edit contact");
+        String name = sc.next();
+        for (Person p :contact) {
+            if(p.getFirstName().equals(name)){
+                System.out.println("Enter first name");
+                p.setFirstName(sc.next());
+                System.out.println("Enter last name");
+                p.setLastName(sc.next());
+                System.out.println("Enter address");
+                p.setAddress(sc.next());
+                System.out.println("Enter city");
+                p.setCity(sc.next());
+                System.out.println("Enter state");
+                p.setState(sc.next());
+                System.out.println("Enter zip code");
+                p.setZipCode(sc.next());
+                System.out.println("Enter phone no");
+                p.setPhoneNumber(sc.next());
+                System.out.println("Enter email-id");
+                p.setEmailId(sc.next());
+            }
+        }
+    }
+
+    private void addToContactList(Person p){
+        Scanner sc = new Scanner(System.in);
+        contact.add(p);
+        System.out.println("Press 1 to add more contact");
+        int add = sc.nextInt();
+        if(add == 1){createContact();}
     }
 
     private void addToAddressBook(List<Person> personList) {
